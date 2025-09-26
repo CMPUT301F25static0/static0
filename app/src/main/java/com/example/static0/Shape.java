ee16c0f1051e54db24168dc3f1f7fd5572fde072
jrdanie1 <jrdanie1@ualberta.ca>
2025-09-25 18:29:08 -0600

    conflict

--- app/src/main/java/com/example/static0/Shape.java
+++ app/src/main/java/com/example/static0/Shape.java
@@ -3,5 +3,5 @@ package com.example.static0;
 public abstract class Shape {
     int x;
     int y;
-    String color = "red"; // this is colour red
+    String color = "black"; // this is colour red
 }
